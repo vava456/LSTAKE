27bf2c801b4cf7aa937e61095a74cbe338a43488
vava456 <vava456@hotmail.fr>
2024-02-21 01:17:28 +0100

    Update Lstake.sol

--- Lstake.sol
+++ Lstake.sol
@@ -2,7 +2,7 @@
 // contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/ERC20.sol";
 
 contract LSTAKE is ERC20 {
     constructor(uint256 initialSupply) public ERC20("LiquiStake", "LSTAKE") {
