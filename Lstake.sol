930d12a342387a8dda9266ecb6ead68b39c2c245
vava456 <vava456@hotmail.fr>
2024-02-21 01:41:37 +0100

    Update Lstake.sol

--- Lstake.sol
+++ Lstake.sol
@@ -1,12 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+/ SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.1;
-
 contract LSTAKE {
-    string public name;
-    string public symbol;
-    
-    constructor(string memory _name, string memory _symbol) {
-        name = _name;
-        symbol = _symbol;
-    }
 }
