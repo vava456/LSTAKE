2d825481ff6b9a7bc4ec3d835b933a651f9404df
vava456 <vava456@hotmail.fr>
2024-02-21 01:15:44 +0100

    Update Lstake.sol

--- Lstake.sol
+++ Lstake.sol
@@ -1,29 +1,11 @@
-// SPDX-License-Identifier: GPL-3.0
 
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/**
- * @title LSTAKE
- * @dev LSTAKE TOKEN V1
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
- */
-contract LSTAKE {
-
-    uint256 number;
-
-    /**
-     * @dev Store value in variable
-     * @param num value to store
-     */
-    function store(uint256 num) public {
-        number = num;
-    }
-
-    /**
-     * @dev Return value 
-     * @return value of 'number'
-     */
-    function retrieve() public view returns (uint256){
-        return number;
+contract LSTAKE is ERC20 {
+    constructor(uint256 initialSupply) public ERC20("LiquiStake", "LSTAKE") {
+        _mint(msg.sender, initialSupply);
     }
 }
